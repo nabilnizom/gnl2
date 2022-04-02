069ee4996b818ad3496fd9b97d2cb237436aa395
Ahmad Nabil <unclebrendi@Ahmads-MacBook-Pro.local>
2022-04-02 23:43:03 +0800

    get_next_line.c debugging segfault

--- get_next_line.c
+++ get_next_line.c
@@ -2,13 +2,13 @@
 #include <stdlib.h>
 #include "get_next_line.h"
 
-#include <fcntl.h>
-#include <stdio.h>
-
 char	*rd_to_ret(char *unret, char *rd, int i)
 {
-	unret = ft_strjoin(unret, &rd[i]);
-	rd[i] = 0;
+	if (ft_strchr(rd, '\n') == &rd[i])
+	{
+		unret = ft_strjoin(unret, &rd[i]);
+		rd[i] = 0;
+	}
 	return (unret);
 }
 
@@ -20,18 +20,18 @@ char	*get_next_line(int fd)
 	static char	*unret = NULL;
 
 	if (unret != NULL)
+	{
 		ret = ft_strdup(unret);
-	free(unret);
+		free(unret);
+	}
+	ret = malloc(sizeof(char) * (4 + 1));
 	unret = malloc(sizeof(char) * (4 + 1));
 	rd = malloc(sizeof(char) * (4 + 1));
 	while (read(fd, rd, 4 && ft_strlen(unret) == 0))
 	{
 		i = -1;
 		while (i++ <= 4 && ft_strlen(unret) == 0)
-		{
-			if (ft_strchr(rd, '\n') == &rd[i])
 				unret = rd_to_ret(unret, rd, i);
-		}
 		ret = ft_strjoin(ret, rd);
 		ft_bzero(rd, 4);
 	}
