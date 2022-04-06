761e59160d88e4a4e2fe19471325e29f469a5347
nabilnizom <75904032+nabilnizom@users.noreply.github.com>
2022-04-06 23:02:44 +0800

    Add files via upload
    Norminette

--- get_next_line_utils.c
+++ get_next_line_utils.c
@@ -1,79 +1,79 @@
 #include "get_next_line.h"
 
-size_t    ft_strlen(const char *str)
+size_t	ft_strlen(const char *str)
 {
-    int    i;
+	int	;
 
-    i = 0;
-    while (str[i])
-        i++;
-    return (i);
+	i = 0;
+	while (str[i])
+		i++;
+	return (i);
 }
 
-char    *ft_strchr(const char *str, int c)
+char	*ft_strchr(const char *str, int c)
 {
-    char    *ptr;
+	char	*ptr;
 
-    ptr = (char*)str;
-    while (*ptr)
-    {
-        if (*ptr == c)
-            return(ptr);
-        ptr++;
-    }
-    return (NULL);
+	ptr = (char*)str;
+	while (*ptr)
+	{
+		if (*ptr == c)
+			return(ptr);
+		ptr++;
+	}
+	return (NULL);
 }
 
-char    *ft_strjoin(char const *s1, char const *s2)
+char	*ft_strjoin(char const *s1, char const *s2)
 {
-    int        len_s1;
-    int        len_s2;
-    char    *sjoin;
-    int        i;
+	int		len_s1;
+	int		len_s2;
+	char	*sjoin;
+	int		i;
 
-    len_s1 = ft_strlen((char*)s1);
-    len_s2 = ft_strlen((char*)s2);
-    sjoin = (char*)malloc(sizeof(char) * (len_s1 + len_s2 + 1));
-    i = 0;
-    while (i < len_s1)
-    {
-        sjoin[i] = s1[i];
-        i++;
-    }
-    while (i < (len_s1 + len_s2))
-    {
-        sjoin[i] = s2[i - len_s1];
-        i++;
-    }
-    sjoin[i] = 0;
-    return(sjoin);
+	len_s1 = ft_strlen((char*)s1);
+	len_s2 = ft_strlen((char*)s2);
+	sjoin = (char*)malloc(sizeof(char) * (len_s1 + len_s2 + 1));
+	i = 0;
+	while (i < len_s1)
+	{
+		sjoin[i] = s1[i];
+		i++;
+	}
+	while (i < (len_s1 + len_s2))
+	{
+	sjoin[i] = s2[i - len_s1];
+		i++;
+	}
+	sjoin[i] = 0;
+	return(sjoin);
 }
 
-void ft_bzero(void *str, size_t n)
+void	ft_bzero(void *str, size_t n)
 {
-    size_t    i;
+	size_t	i;
 
-    i = 0;
-    while (i < n)
-        ((unsigned char*)str)[i++] = '\0';
+	i = 0;
+	while (i < n)
+		((unsigned char*)str)[i++] = '\0';
 }
 
-char    *ft_strdup(const char *str)
+char	*ft_strdup(const char *str)
 {
-    char    *ptr;
-    int        len;
-    int        i;
+	char	*ptr;
+	int		len;
+	int		i;
 
-    len = ft_strlen((char *)str);
-    ptr = (char *)malloc(sizeof(char) * (len + 1));
-    i = 0;
-    if (ptr == NULL)
-        return (NULL);
-    while (str[i])
-    {
-        ptr[i] = str[i];
-        i++;
-    }
-    ptr[i] = '\0';
-    return (ptr);
+	len = ft_strlen((char *)str);
+	ptr = (char *)malloc(sizeof(char) * (len + 1));
+	i = 0;
+	if (ptr == NULL)
+		return (NULL);
+	while (str[i])
+	{
+		ptr[i] = str[i];
+		i++;
+	}
+	ptr[i] = '\0';
+	return (ptr);
 }
